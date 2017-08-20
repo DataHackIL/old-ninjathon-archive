2a7d369f1ee2dfa9b535b4c2a057d7c988d673dd
shakedlokits <shaked.lokits@gmail.com>
2017-08-20 17:10:19 +0300

    added horizon, CD using AWS EC2
    fixed travis secret key
    
    fixed compatability issues with concurrently
    
    added cd file deployment and permissions
    
    fixing grep command
    
    fixing node install command
    
    linted yaml file
    
    added codedeploy to travis
    
    linted travis yaml and added secret
    
    removed node modules before deploy
    
    remove codedeploy
    
    added manual CD to aws EC2
    
    reencrypted secrets file
    
    restashed secrets
    
    restashed secrets
    
    restashed secrets
    
    allowed ssh to hosts
    
    changed ssh permissions
    
    made permission changes on deploy script

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './App'
 import {BrowserRouter as Router} from 'react-router-dom'
+import { HorizonProvider, Horizon } from 'react-hz'
+
+const horizonInstance = Horizon()
 
 render((
-  <Router>
-    <App/>
-  </Router>
+	<HorizonProvider instance={horizonInstance}>
+		<Router>
+			<App/>
+		</Router>
+	</HorizonProvider>
 ), document.getElementById('app'))
